814f8261ef0fcec40d075024e985181ff8a6c3f4
BALAJI MS <110657900+balaji-ms@users.noreply.github.com>
2023-03-07 13:29:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -20,10 +20,10 @@ credentials = service_account.Credentials.from_service_account_file("credentials
 def index() -> str:
     now = datetime.now()
     date_tdy=str(now.date())
-    datetime_str = date_tdy+' '+'5:00:00'
+    datetime_str = date_tdy+' '+'8:00:00'
     datetime_object = datetime.strptime(datetime_str, '%Y-%m-%d %H:%M:%S')
 
-    datetime_str1 = date_tdy+' '+'6:10:00'
+    datetime_str1 = date_tdy+' '+'10:00:00'
     datetime_object1 = datetime.strptime(datetime_str1, '%Y-%m-%d %H:%M:%S')
 
 
